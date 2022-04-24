c988a56fc1fc1c6fda16d51b2f4e8c2a6d56f926
Kimmi Lee <lee.kimmixq@gmail.com>
2022-04-25 01:53:11 +0800

    added bills controller to routes with post bill route

--- routes.mjs
+++ routes.mjs
@@ -1,6 +1,12 @@
 import { resolve } from 'path';
+import db from './database/models/index.mjs';
+
+import initBillsController from './database/controllers/bills.mjs';
 
 export default function routes(app) {
+  const BillsController = initBillsController(db);
+  app.post('/bill', BillsController.postBill);
+
   // Root route renders Webpack-generated main.html file
   app.get('/', (request, response) => {
     response.sendFile(resolve('dist', 'main.html'));
