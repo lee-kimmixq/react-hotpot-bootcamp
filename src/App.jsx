291189ab47a8752e1ca805021a28f4739db3d5e9
Kimmi Lee <lee.kimmixq@gmail.com>
2022-04-25 15:13:46 +0800

    created temporary back button to reset currentBill

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,17 @@ export default function App() {
   return (
     <div className="container">
       {!currentBill && <BillForm setCurrentBill={setCurrentBill} />}
+      {currentBill && (
+      <button
+        type="button"
+        className="btn btn-secondary"
+        onClick={() => {
+          setCurrentBill();
+        }}
+      >
+        Back
+      </button>
+      )}
     </div>
   );
 }
