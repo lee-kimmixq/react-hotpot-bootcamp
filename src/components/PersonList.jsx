5f53b71304932b63ab88d4de8c7691631d53c132
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-07 08:15:01 +0800

    created new component to show list of people and amount owed (from database)

---
+++ src/components/PersonList.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function PersonList({ currentBillId }) {
+  const [personList, setPersonList] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/person/${currentBillId}`)
+      .then((res) => {
+        const newPersonList = res.data.people.map(({ id, name, amount }) => (
+          <li key={id}>
+            {name}
+            {' '}
+            -
+            {' '}
+            <b>
+              $
+              {amount}
+            </b>
+          </li>
+        ));
+        setPersonList(newPersonList);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h3>Amount Owed Person List</h3>
+      <div><ul>{personList}</ul></div>
+    </div>
+  );
+}
