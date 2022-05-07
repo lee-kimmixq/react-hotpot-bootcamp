116421cfbda0944824ec7d607da173747067669a
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-07 08:14:11 +0800

    added people controller to routes.mjs and created new route for get all people

--- routes.mjs
+++ routes.mjs
@@ -2,12 +2,17 @@ import { resolve } from 'path';
 import db from './database/models/index.mjs';
 
 import initBillsController from './database/controllers/bills.mjs';
+import initPeopleController from './database/controllers/people.mjs';
 
 export default function routes(app) {
   const BillsController = initBillsController(db);
+  const PeopleController = initPeopleController(db);
+
   app.post('/bill', BillsController.postBill);
   app.get('/bills', BillsController.getAllBills);
   app.get('/bill/:id', BillsController.getBillFromId);
+
+  app.get('/person/:id', PeopleController.getAllPeople);
 
   // Root route renders Webpack-generated main.html file
   app.get('/', (request, response) => {
